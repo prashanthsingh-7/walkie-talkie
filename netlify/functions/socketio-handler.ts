4bf48f09643ccbc1ab1b422782fa83a4acb85840
Prashanth <prashanth.singh@mca.christuniversity.in>
2025-03-30 18:14:06 +0530

    Update Socket.IO configuration for Netlify Functions

--- netlify/functions/socketio-handler.ts
+++ netlify/functions/socketio-handler.ts
@@ -7,7 +7,13 @@ const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
+    credentials: true
   },
+  path: "/socket.io/",
+  transports: ['websocket', 'polling'],
+  allowEIO3: true,
+  pingTimeout: 60000,
+  pingInterval: 25000
 })
 
 interface User {
